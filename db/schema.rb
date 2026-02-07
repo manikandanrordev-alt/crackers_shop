dbe37b5d698ae559e874ba65e15d09a979e928e5
manikandan <manikandan.rordev@gmail.com>
2026-02-07 19:08:32 +0530

    schema file added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_07_055132) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_07_140000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -100,6 +100,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_07_055132) do
 
   create_table "shop_configs", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.decimal "default_discount_percentage", precision: 5, scale: 2
     t.string "facebook_url"
     t.string "instagram_url"
     t.string "key"
